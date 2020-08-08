23bf4e80dde8c1eea9f3d3d53f2f402a23f0b26c
luisliquin <luis_liquin@hotmail.com>
2020-08-08 14:46:05 -0300

    arraylist

---
+++ certificacion c#/8-37/Array_List/Array_List/Clientes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Array_List {
+    public class Clientes {
+        private ArrayList db = new ArrayList();
+
+        public void Agregar(string nombre) {
+            db.Add(nombre);
+        }
+
+        public void AgregarLista(string[] clientes) {
+            db.AddRange(clientes);
+        }
+
+        public void Editar(string edit, string new_element) {
+            db[db.IndexOf(edit)] = new_element;
+        }
+
+        public void Eliminar(string elemento) {
+            db.Remove(elemento);
+        }
+
+        public void SelectClientes() {
+            foreach (object db in db) {
+                Console.WriteLine(db.ToString());
+            }
+        }
+
+        public void Ordenar() {
+            db.Sort(new OrdernarDesc());
+        }
+    }
+
+    class OrdernarDesc : IComparer {
+        public int Compare(object x, object y) {
+            return (new CaseInsensitiveComparer().Compare(y, x));
+        }
+    }
+}
---
+++ certificacion c#/8-37/Array_List/Array_List/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Array_List {
+    class Program {
+        static void Main(string[] args) {
+            //ArrayList arli = new ArrayList();
+            //arli.Add(2);
+            //arli.Add("dos");
+            //arli.Add(12.5);
+            //arli.Insert(1, true);
+            //arli.RemoveAt(2);
+            //arli.Remove(2);
+            //Console.WriteLine("la coleccion tienen " + arli.Count + "elementos");                        
+            //foreach (object a in arli) {
+            //    Console.WriteLine(a.ToString());
+            //}
+
+            Clientes cli = new Clientes();
+            cli.Agregar("jose perez");
+            cli.AgregarLista(new string[] {"cliente2", "cliente3", "cliente4" });
+            cli.Editar("cliente2", "Maria Sanchez");
+            cli.Eliminar("cliente4");
+            cli.Ordenar();
+            cli.SelectClientes();
+            Console.ReadKey();
+        }
+    }
+}
