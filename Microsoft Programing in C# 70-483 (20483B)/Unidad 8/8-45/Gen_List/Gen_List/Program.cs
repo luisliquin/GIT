3f531b862fb85cad464cae3eeece7b62b76769c5
luisliquin <luis_liquin@hotmail.com>
2020-08-09 14:35:47 -0300

    Curso C#

---
+++ Microsoft Programing in C# 70-483 (20483B)/Unidad 8/8-45/Gen_List/Gen_List/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gen_List {
+    class Program {
+        static void Main(string[] args) {
+            List<Producto> listPro = new List<Producto>();
+            listPro.Add(new Producto("pro001", "avena", 6.23));
+            listPro.Add(new Producto("pro002", "leche", 1.87));
+            listPro.Add(new Producto("pro003", "mermelada", 6.23));
+            listPro.Add(new Producto("pro004", "avena", 6.23));
+
+            listPro.Sort();
+
+            foreach (Producto p in listPro) {
+                Console.WriteLine(p + ", " + p.Precio.ToString());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Microsoft Programing in C# 70-483 (20483B)/Unidad 8/8-49/Gen_Dictionary/Gen_Dictionary/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gen_Dictionary {
+    class Program {
+        static void Main(string[] args) {
+            //Dictionary<string, string> midic = new Dictionary<string, string>();
+
+            IDictionary<string, string> midic = new Dictionary<string, string>();
+            midic.Add("clave1", "valor1");
+            midic.Add("clave2", "valor2");
+            midic.Add("clave3", "valor3");
+
+            midic.Add(new KeyValuePair<string, string>("1","uno"));
+
+            foreach (KeyValuePair<string, string> elemento in midic) {
+                Console.WriteLine("keys: {0} valores {1}", elemento.Key.ToString(), elemento.Value.ToString());
+            }
+
+            string val;
+
+            if (midic.TryGetValue("clave", out val)) {
+                Console.WriteLine("el valor se ha encontrado y es: " + val);
+            }
+            else {
+                Console.WriteLine("error");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
