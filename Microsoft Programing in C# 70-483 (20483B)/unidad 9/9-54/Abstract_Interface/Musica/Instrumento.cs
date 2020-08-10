969de1116fa9dc0264e4f148ae8bee4753ab54d7
luisliquin <luis_liquin@hotmail.com>
2020-08-10 19:43:37 -0300

    Clase Abstracta

---
+++ Microsoft Programing in C# 70-483 (20483B)/unidad 9/9-54/Abstract_Interface/Musica/Instrumento.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Musica {
+    public abstract class Instrumento {
+        public Instrumento() {            
+        }
+        public abstract string Tocar();        
+    }
+
+    public class Guitarra : Instrumento {
+        public Guitarra() {
+        }
+        public override string Tocar() {
+            return "Guitarra";
+        }
+    }
+
+    public class Violin : Instrumento {
+        public override string Tocar() {
+            return "violin";
+        }
+    }
+}
---
+++ Microsoft Programing in C# 70-483 (20483B)/unidad 9/9-54/Abstract_Interface/Musica/Vehiculo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Musica {
+    public class Vehiculo : IFuncion {
+
+        public Vehiculo() { }
+
+        private string _Nombre;
+        
+        public string Nombre {
+            get {
+                return _Nombre;
+            }
+            set {
+                _Nombre=value;
+            }
+        }
+
+        public string Encender() {
+            return "Prendido";
+        }
+    }
+}
