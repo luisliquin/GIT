db36f4a4a4afdb3d2136cb35dcedb2630f08c68e
luisliquin <luis_liquin@hotmail.com>
2020-08-12 01:34:24 -0300

    LecturaEscritura

--- Microsoft Programing in C# 70-483 (20483B)/unidad 10/LecturaEscritura/LecturaEscritura/Form1.Designer.cs
+++ Microsoft Programing in C# 70-483 (20483B)/unidad 10/LecturaEscritura/LecturaEscritura/Form1.Designer.cs
@@ -23,21 +23,34 @@
         /// el contenido de este método con el editor de código.
         /// </summary>
         private void InitializeComponent() {
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(407, 199);
+            this.textBox1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(472, 276);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Microsoft Programing in C# 70-483 (20483B)/unidad 10/LecturaEscritura/LecturaEscritura/Form1.cs
+++ Microsoft Programing in C# 70-483 (20483B)/unidad 10/LecturaEscritura/LecturaEscritura/Form1.cs
@@ -11,12 +11,26 @@ using System.IO;
 
 namespace LecturaEscritura {
     public partial class Form1 : Form {
+
+        string ruta = Application.StartupPath + @"\archivo.txt";
         public Form1() {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e) {
+            //Stream st = File.OpenRead(ruta);
+            //BufferedStream buf = new BufferedStream(st);
+            //byte[] b = new byte[128];        
+            //buf.Read(b,0,128);            
+            //MessageBox.Show("el arreglo contiene " + Encoding.ASCII.GetString(b));
 
+            StreamWriter wri = File.AppendText(ruta);
+            wri.Write("Mas texto");
+            wri.Close();
+
+            StreamReader re = new StreamReader(ruta);
+            textBox1.Text = re.ReadToEnd();
+            re.Close();
         }
     }
 }
