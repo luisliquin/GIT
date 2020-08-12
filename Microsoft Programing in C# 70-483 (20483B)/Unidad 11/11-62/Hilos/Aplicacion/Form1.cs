426d9336c00a4484d5478bc6a7f1748f953a7160
luisliquin <luis_liquin@hotmail.com>
2020-08-12 20:20:03 -0300

    Modificaciones

---
+++ Microsoft Programing in C# 70-483 (20483B)/Unidad 11/11-62/Hilos/Aplicacion/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Aplicacion {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e) {
+            CorrerProceso();
+        }
+        private void CambiarProgreso(int valor) {
+            progressBar.Value=valor;
+        }
+
+        private void CorrerProceso() {
+            for (int c = 1; c <= progressBar.Maximum; c++) {
+                Thread.Sleep(1000);
+                CambiarProgreso(c);
+            }
+            Console.WriteLine("proceso finalizado");
+        }
+    }
+
+    
+
+}
---
+++ Microsoft Programing in C# 70-483 (20483B)/Unidad 11/11-62/Hilos/Hilos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Hilos {
+    class Program {
+        static void Main(string[] args) {
+            Thread hilo1 = new Thread(new ThreadStart(Tarea1));
+            Thread hilo2 = new Thread(new ThreadStart(Tarea2));
+            hilo1.Start();
+            hilo2.Start();
+            hilo1.Priority=ThreadPriority.Highest;
+            hilo1.Priority=ThreadPriority.Lowest;
+            Console.ReadKey();
+        }
+
+        static private void Metodo() {
+            Console.WriteLine("inicio del proceso 2");
+            Console.WriteLine("termino del proceso 2");
+        }
+
+        static private void Tarea1() {
+            for (int x = 1; x<=10; x++) {
+                Thread.Sleep(1000);
+                Console.WriteLine("{0}",x);
+            }
+        }
+
+        static private void Tarea2() {
+            for (int x = 11; x<=20; x++) {
+                Thread.Sleep(1000);
+                Console.WriteLine("{0}", x);
+            }
+        }
+    }
+}
