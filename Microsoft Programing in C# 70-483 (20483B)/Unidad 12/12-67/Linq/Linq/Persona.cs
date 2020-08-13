0e15247ace1d841a07ee87bf3a28f55ed68c4263
luisliquin <luis_liquin@hotmail.com>
2020-08-13 20:55:53 -0300

    Linq

---
+++ Microsoft Programing in C# 70-483 (20483B)/Unidad 12/12-67/Linq/Linq/Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq {
+    class Persona {
+        private string _nombre;
+        private string _apellido;
+        private int _edad;
+
+        public Persona(string nom, string ape, int edad) {
+            this._nombre= nom;
+            this._apellido=ape;
+            this._edad=edad;
+        }
+
+        public string Nombre { get { return _nombre;}}
+        public string Apellido { get { return _apellido;}}
+        public int Edad { get { return _edad; }}
+    }
+}
