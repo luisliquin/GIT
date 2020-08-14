3310182940ff12b04c07e840d0a96b24f9f95172
luisliquin <luis_liquin@hotmail.com>
2020-08-14 15:30:51 -0300

    Remoto

---
+++ Microsoft Programing in C# 70-483 (20483B)/Unidad 13/13-73/ConexionRemota/ConexionRemota/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ConexionRemota {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void RecuperarDatos (string url){
+            WebClient webClient = new WebClient();
+            Stream strean  = webClient.OpenRead(url);
+            
+            StreamReader reader = new StreamReader(strean);
+            string rpta = reader.ReadToEnd();
+            reader.Close();
+
+            richTextBox.Text = rpta;
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e) {
+            RecuperarDatos(txtURL.Text);
+        }
+    }
+}
