c4b2eeff66719be4710e6c485904cd0770eeb165
luisliquin <luis_liquin@hotmail.com>
2020-08-15 12:47:26 -0300

    Sockets

---
+++ Microsoft Programing in C# 70-483 (20483B)/Unidad 13/13-73/Sockets/Cliente/Cliente/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cliente {
+    class Program {
+        static void Main(string[] args) {
+            Conectar();
+        }
+
+        static void Conectar() {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint direccion = new IPEndPoint(IPAddress.Parse("192.168.1.7"),80);
+
+            string mensaje;
+            byte[] aEnviar;
+            
+            try {
+                socket.Connect(direccion);
+
+                Console.WriteLine("Cliente conectado con exito... ");
+
+                Console.WriteLine("Ingrese mensaje ");
+                mensaje = Console.ReadLine();
+                aEnviar = Encoding.Default.GetBytes(mensaje);
+                socket.Send(aEnviar, 0, aEnviar.Length, 0);
+
+                Console.WriteLine("mensaje enviado");
+                socket.Close();
+            }
+            catch (Exception exc) {
+                Console.WriteLine("ERROR "+exc.Message.ToString());
+            }
+
+            Console.WriteLine("presione una tecla...");
+            Console.ReadKey();
+        }
+    }
+}
