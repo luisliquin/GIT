94b9ea3ad27963fb67ac4a2ac792f2be2a34faec
luisliquin <luis_liquin@hotmail.com>
2020-08-16 04:48:53 -0300

    MetodoAnonimo

---
+++ Microsoft Programing in C# 70-483 (20483B)/Unidad 18/MetodoAnonimo/MetodoAnonimo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoAnonimo {
+    class Program {
+
+        delegate void ShowMessageDelegate();
+        
+        static void Main(string[] args) {
+            Action E = ShowMessage;
+            E();
+
+            Action D = delegate(){
+                Console.WriteLine("mensaje metodo anonimo");
+            };
+            D();
+
+            ShowMessageDelegate C = delegate(){
+                Console.WriteLine("delegate metodo anonimo");
+            };
+            C();
+
+            Console.ReadKey();
+        }
+
+        static void ShowMessage() {
+            Console.WriteLine("mensaje");
+        }
+    }
+}
