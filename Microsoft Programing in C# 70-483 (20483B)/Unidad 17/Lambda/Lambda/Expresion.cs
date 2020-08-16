2146f6590710b14cf685451ca2f1fec68cf84bfe
luisliquin <luis_liquin@hotmail.com>
2020-08-16 04:30:17 -0300

    Lambda

---
+++ Microsoft Programing in C# 70-483 (20483B)/Unidad 17/Lambda/Lambda/Expresion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lambda {
+    class Expresion {
+        static void Main(string[] args) {
+            //Func<string, int, int, decimal> H;
+            //H=(msg, a, b) => Add(msg, a, b);
+            //Console.WriteLine(H("mensaje", 5, 5));
+
+            Func<string, int, int, decimal> V;
+            V=(msg, a, b) => {
+                Console.WriteLine($"{msg}: {a} + {b}");
+                return a+b;
+            };
+
+            Console.WriteLine(  V("mensaje 2", 10, 10));
+
+            Console.ReadKey();
+        }
+
+        static decimal Add(string msg, int a, int b) {
+            Console.WriteLine($"{msg}: {a} + {b}");
+            return a+b;
+        }
+    }
+}
