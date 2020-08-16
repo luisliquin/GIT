6eacb0d91bb0a088329041d29ec3f46eab7c6ef7
luisliquin <luis_liquin@hotmail.com>
2020-08-16 04:10:28 -0300

    Delegado

---
+++ Microsoft Programing in C# 70-483 (20483B)/Unidad 16/Ejemplo/Operacion/Division.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operacion
+{
+    public class Division  {
+        public Action<int,int> MessageHandler;
+        public int Divide(int a, int b) {
+            int result = 0;
+            if (b!=0) {
+                result=a/b;
+            }
+            else {
+                MessageHandler(a, b);
+            }
+            return result;
+        }
+    }
+}
