96f6b57b81512bec013f2048c1c96b5496b83d27
luisliquin <luis_liquin@hotmail.com>
2020-10-03 10:56:58 -0300

    Practica

---
+++ 02 Desarrollo Web desde 0 2020/Seccion 3/12 Arrays/JS/script.js
@@ -0,0 +1,11 @@
+let numeros=[1,2,3,4,5,6];
+
+let palabras=['hola', 'estamos', 'en','youtube'];
+
+let booleans=[true, false, true];
+
+console.log(numeros[0]+numeros[1]);
+
+console.log(`La palabra ${palabras[1]} tiene ${palabras[1].length} letras.`);
+
+console.log(numeros);
---
+++ 02 Desarrollo Web desde 0 2020/Seccion 3/13 Arrays II/JS/script.js
@@ -0,0 +1,40 @@
+let numbers = [1,2,3,4,5];
+/*
+console.log(numbers.length);
+
+let number = 4;
+console.log(Array.isArray(number));
+console.log(Array.isArray(numbers));
+
+console.log(numbers);
+let deleteElement = numbers.shift();
+console.log(numbers);
+console.log(deleteElement);
+console.log(numbers);
+numbers.pop();
+console.log(numbers);
+
+let newLength = numbers.push(6);
+console.log(numbers);
+let newLenght2 = numbers.unshift(0);
+console.log(numbers);
+
+console.log(numbers);
+console.log(numbers.indexOf(1));
+console.log(numbers);
+console.log(numbers.lastIndexOf(1));
+
+console.log(numbers);
+numbers.reverse();
+console.log(numbers);
+
+console.log(numbers);
+let arrayString = numbers.join(' ');
+console.log(arrayString);
+console.log(numbers.join('-'));
+
+console.log(numbers);
+numbers.splice(3); -elimina desde la posicion 3 hasta el final
+numbers.splice(2,2); -elimina desde la posicion 2 sus 2 siguientes
+console.log(numbers);
+*/
