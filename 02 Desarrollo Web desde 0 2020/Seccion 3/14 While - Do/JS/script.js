857a6ef33e48bc8dce3de969a582fdeb04f7fa1c
luisliquin <luis_liquin@hotmail.com>
2020-10-04 20:24:23 -0300

    while do

---
+++ 02 Desarrollo Web desde 0 2020/Seccion 3/14 While - Do/JS/script.js
@@ -0,0 +1,12 @@
+
+let pass = 'hola';
+
+while(pass != 'hola'){
+    pass = prompt (`introduzca una contraseña`);
+}
+console.log("fin del bucle");
+
+do{
+    pass = prompt (`introduzca una contraseña`);
+}while(pass != 'hola');
+console.log("fin del bucle");
