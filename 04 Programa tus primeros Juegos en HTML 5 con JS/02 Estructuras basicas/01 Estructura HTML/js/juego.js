1a9d343913f7f0bc3e5906beadca5b2cedc66cd0
luisliquin <luis_liquin@hotmail.com>
2021-01-25 09:49:01 -0300

    Actualizacion Git

---
+++ 04 Programa tus primeros Juegos en HTML 5 con JS/02 Estructuras basicas/01 Estructura HTML/js/juego.js
@@ -0,0 +1,16 @@
+var turno = 1; 
+var vida = 100;
+
+//Estado jugador
+var vivo = true;
+
+//jugada cpu
+var atacar = 0;
+var quemar = 1;
+var envenenar = 2;
+var fallar = 3;
+
+function juegaTurno(){
+    var jugadaCPU = Math.floor(Math.random() * 4);
+    //console.log(jugadaCPU);
+}
