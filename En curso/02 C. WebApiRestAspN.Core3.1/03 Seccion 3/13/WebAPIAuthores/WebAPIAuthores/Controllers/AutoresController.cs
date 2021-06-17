b2f3314bce5836cd2e33c9e722ef2f7f1b02493c
luisliquin <luis_liquin@hotmail.com>
2021-06-17 20:53:33 -0300

    update

---
+++ En curso/02 C. WebApiRestAspN.Core3.1/03 Seccion 3/13/WebAPIAuthores/WebAPIAuthores/Controllers/AutoresController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebAPIAuthores.Entidades;
+using System.Data.Entity.DbContext;
+
+namespace WebAPIAuthores.Controllers {
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController : ControllerBase{
+
+        private readonly ApplicationDbContext context;
+
+        public AutoresController(ApplicationDBContext context) {
+            this.context=context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Autor>> Get() {
+            return new List<Autor>() {
+                new Autor() { Id=1, Nombre="Felipe" },
+                new Autor() { Id=2, Nombre="Claudia" }
+            };
+        }
+
+        [HttpPost]
+        public async Task<ActionResult>Post(Autor autor) {
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
