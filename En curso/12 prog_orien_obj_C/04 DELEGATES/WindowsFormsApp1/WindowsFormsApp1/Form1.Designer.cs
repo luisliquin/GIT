370029f21b193b3beee5ce6dacbb3a0a854b9586
luisliquin <luis_liquin@hotmail.com>
2021-06-20 19:15:25 -0300

    Vue_NetCore

--- En curso/12 prog_orien_obj_C/04 DELEGATES/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ En curso/12 prog_orien_obj_C/04 DELEGATES/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -33,8 +33,9 @@ namespace WindowsFormsApp1 {
             this.textBox1.Location = new System.Drawing.Point(28, 26);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(187, 188);
+            this.textBox1.Size = new System.Drawing.Size(223, 188);
             this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
@@ -42,18 +43,20 @@ namespace WindowsFormsApp1 {
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Show";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(397, 237);
+            this.ClientSize = new System.Drawing.Size(359, 236);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
