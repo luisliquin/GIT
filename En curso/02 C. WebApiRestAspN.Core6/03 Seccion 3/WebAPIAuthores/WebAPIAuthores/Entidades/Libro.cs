90a8cd380943bc8a790de804e08f4a0857030d7e
luisliquin <luis_liquin@hotmail.com>
2021-07-03 00:37:48 -0300

    Update20210703

---
+++ En curso/02 C. WebApiRestAspN.Core6/03 Seccion 3/WebAPIAuthores/WebAPIAuthores/Entidades/Libro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIAuthores.Entidades {
+    public class Libro {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public int AutorId { get; set; }
+        public Autor Autor { get; set; }
+    }
+}
